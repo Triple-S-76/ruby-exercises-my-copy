3b2f12ea2db05cc16eb50eadfbee7b120f055597
TripleS <triple-s-76@outlook.com>
2022-10-10 03:05:20 -0700

    Finished method exercises

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,23 +7,36 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(num)
+  num.chr
+end
+
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
 
+def common_sports(array_1, array_2)
+  array_1.intersection(array_2)
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(array)
+  array.sort.uniq
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
 
+def lucky_number(number="7")
+  "Today's lucky number is " + number.to_s
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
@@ -31,6 +44,12 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(char)
+  if char.length != 1
+    return "Input Error"
+  end
+  char.ord
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -40,9 +59,28 @@
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+def pet_pun(animal)
+  if animal == "cat"
+    puts "Cats are purr-fect!"
+    return
+  end
+  if animal == "dog"
+    puts "Dogs are paw-some!"
+    return
+  end
+  if animal != "cat" || "dog"
+    puts "I think #{animal}s have pet-tential!"
+  end
+end
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+def twenty_first_century?(year)
+  if year >= 2000 && year <= 2100
+    return true
+  end
+  false
+end
