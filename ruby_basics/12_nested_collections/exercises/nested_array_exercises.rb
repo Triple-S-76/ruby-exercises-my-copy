67ff60be8cd9dca48d6bb6f0699285479cf86f61
TripleS <triple-s-76@outlook.com>
2023-01-02 07:45:37 -0800

    Redid the exercises

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -1,3 +1,83 @@
+# def blank_seating_chart(number_of_rows, seats_per_row)
+#   # return a 2d array to represent a seating chart that contains
+#   # number_of_rows nested arrays, each with seats_per_row entries of nil to
+#   # represent that each seat is empty.
+
+#   # Example: blank_seating_chart(2, 3) should return:
+#   # [
+#   #   [nil, nil, nil],
+#   #   [nil, nil, nil]
+#   # ]
+
+#   # NOTE: if one of the nested arrays is changed, the others should **not**
+#   # change with it
+#   Array.new(number_of_rows) {Array.new(seats_per_row)}
+# end
+
+# def add_seat_to_row(chart, row_index, seat_to_add)
+#   # take a chart (2d array)  and add seat_to_add to the end of the row that is
+#   # at row_index index of the chart, then return the chart
+#   chart[row_index] << seat_to_add
+#   chart
+# end
+
+# def add_another_row(chart, row_to_add)
+#   # take a chart and add row_to_add to the end of the chart,
+#   # then return the chart.
+#   chart << row_to_add
+# end
+
+# def delete_seat_from_row(chart, row_index, seat_index)
+#   # take a chart and delete the seat at seat_index of the row at row_index of
+#   # the chart, then return the chart
+
+#   # Hint: explore the ruby docs to find a method for deleting from an array!
+#   chart[row_index].delete_at(seat_index)
+#   chart
+# end
+
+# def delete_row_from_chart(chart, row_index)
+#   # take a chart and delete the row at row_index of the chart,
+#   # then return the chart
+#   chart.delete_at(row_index)
+#   chart
+# end
+
+# def count_empty_seats(chart)
+#   # take a chart and return the number of empty (nil) seats in it
+
+#   # NOTE: `chart` should **not** be mutated
+#   chart.flatten.count(nil)
+# end
+
+# def find_favorite(array_of_hash_objects)
+#   # take an array_of_hash_objects and return the hash which has the key/value
+#   # pair :is_my_favorite? => true. If no hash returns the value true to the key
+#   # :is_my_favorite? it should return nil
+
+#   # array_of_hash_objects will look something like this:
+#   # [
+#   #   { name: 'Ruby', is_my_favorite?: true },
+#   #   { name: 'JavaScript', is_my_favorite?: false },
+#   #   { name: 'HTML', is_my_favorite?: false }
+#   # ]
+
+#   # TIP: there will only be a maximum of one hash in the array that will
+#   # return true to the :is_my_favorite? key
+#   array_of_hash_objects.each do |array_item|
+#     favorite = nil
+#     array_item.each do |k, v|
+#       favorite = "is found" if v == true
+#     end
+#     return array_item if favorite != nil
+#   end
+#   nil
+# end
+
+
+
+
+
 def blank_seating_chart(number_of_rows, seats_per_row)
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
@@ -11,20 +91,28 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
-  Array.new(number_of_rows) {Array.new(seats_per_row)}
+  new_array = []
+  number_of_rows.times do
+    temp_array = []
+    seats_per_row.times do
+      temp_array << nil
+    end
+    new_array << temp_array
+  end
+  new_array
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
-  chart[row_index] << seat_to_add
+  chart[row_index].append(seat_to_add)
   chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
-  chart << row_to_add
+  chart.append(row_to_add)
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -47,7 +135,11 @@ def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
-  chart.flatten.count(nil)
+  empty = 0
+  chart.each do |row|
+    row.each { |seat| empty += 1 if seat.nil? }
+  end
+  empty
 end
 
 def find_favorite(array_of_hash_objects)
@@ -64,12 +156,8 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
-  array_of_hash_objects.each do |array_item|
-    favorite = nil
-    array_item.each do |k, v|
-      favorite = "is found" if v == true
-    end
-    return array_item if favorite != nil
+  array_of_hash_objects.each do |object|
+    return object if object[:is_my_favorite?] == true
   end
   nil
 end
--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -1,3 +1,87 @@
+# def find_language_information(languages, language_name)
+#   # Take languages (a nested hash) and language_name as a symbol, return the
+#   # value for the language_name key (which will be another hash!)
+
+#   # the languages hash will look something like this:
+#   # {
+#   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
+#   #   javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
+#   # }
+#   languages.each do |k, v|
+#     return v if k == language_name
+#   end
+# end
+
+# def add_information_about_language(languages, language_name, info_key, info_value)
+#   # Take languages and add the key/value pair info_key/info_value to the nested
+#   # hash of language_name, then return the updated languages hash
+#   languages.each do |name, v|
+#     v[info_key] = info_value if name == language_name
+#   end
+# end
+
+# def add_language(languages, language_name, language_info_value)
+#   # Take languages and add the key/value pair language_name/language_info_value
+#   # to it, then return languages
+#   languages[language_name] = language_info_value
+#   languages
+# end
+
+# def delete_information_about_language(languages, language_name, info_key)
+#   # Take languages and delete the key/value pair with key info_key from
+#   # language_name, then return languages
+#   languages.each do |language, v|
+#     v.delete(info_key) if language == language_name
+#   end
+# end
+
+# def delete_language(languages, language_name)
+#   # Take languages and delete the language_name key/value pair, then return
+#   # languages
+#   languages.each do |k, v|
+#     languages.delete(language_name) if k == language_name
+#   end
+# end
+
+# def find_beautiful_languages(languages)
+#   # Take languages and return a hash containing only languages which have the
+#   # key/value pair { is_beautiful?: true } listed in their information
+#   languages.select do |language, v|
+#     keep = false
+#     v.each do |k, v|
+#       keep = true if v == true
+#     end
+#     keep
+#   end
+# end
+
+# def find_language_facts(languages, language_name, fact_index = 0)
+#   # Take languages (now with additional facts added to each language with the
+#   # key :facts and value of an array of strings) and return the fact
+#   # language_name has at fact_index of its facts array, or at index 0 if this
+#   # argument is not given
+
+#   # the revised languages hash will look something like this:
+#   # {
+#   #   ruby: { facts: ['fact 0', 'fact 1'],
+#   #           initial_release: 'December 25, 1996',
+#   #           is_beautiful?: true },
+
+#   #   javascript: { facts: ['fact 0', 'fact 1'],
+#   #                 initial_release: 'December 4, 1995',
+#   #                 is_beautiful?: false }
+#   # }
+#   languages.each do |key_1, value_1|
+#     value_1.each do |key_2, value_2|
+#       return value_2[fact_index] if key_1 == language_name
+#     end
+#   end
+# end
+
+
+
+
+
 def find_language_information(languages, language_name)
   # Take languages (a nested hash) and language_name as a symbol, return the
   # value for the language_name key (which will be another hash!)
@@ -7,17 +91,15 @@ def find_language_information(languages, language_name)
   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
   #   javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
   # }
-  languages.each do |k, v|
-    return v if k == language_name
-  end
+    lang = languages.select { |language| language == language_name }
+    lang[language_name]
 end
 
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
   # hash of language_name, then return the updated languages hash
-  languages.each do |name, v|
-    v[info_key] = info_value if name == language_name
-  end
+  languages[language_name].merge!(info_key => info_value)
+  languages
 end
 
 def add_language(languages, language_name, language_info_value)
@@ -30,29 +112,21 @@ end
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
   # language_name, then return languages
-  languages.each do |language, v|
-    v.delete(info_key) if language == language_name
-  end
+  languages[language_name].delete(info_key)
+  languages
 end
 
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
   # languages
-  languages.each do |k, v|
-    languages.delete(language_name) if k == language_name
-  end
+  languages.delete(language_name)
+  languages
 end
 
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
-  languages.select do |language, v|
-    keep = false
-    v.each do |k, v|
-      keep = true if v == true
-    end
-    keep
-  end
+  languages.select { |key, value| value[:is_beautiful?] == true }
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
@@ -71,9 +145,5 @@ def find_language_facts(languages, language_name, fact_index = 0)
   #                 initial_release: 'December 4, 1995',
   #                 is_beautiful?: false }
   # }
-  languages.each do |key_1, value_1|
-    value_1.each do |key_2, value_2|
-      return value_2[fact_index] if key_1 == language_name
-    end
-  end
+  languages.select { |key, value| return value[:facts][fact_index] if key == language_name }
 end
